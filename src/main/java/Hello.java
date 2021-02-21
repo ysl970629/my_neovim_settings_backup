b8102db3ed3baf06668e9e66bafce6afd13df6ce
ysl2 <songli.yu@outlook.com>
2021-02-21 10:00:51 +0800

    2021-02-21 10:00:51

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
